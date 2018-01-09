f0389831c1de19646d2939d7c840dac5a62cd5f0
Hyden Zhang <hyden.zhang@temple.edu>
2018-01-09 12:05:38 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,14 @@
+#Probably need to run at least one sudo command otherwise the script will get held up by requiring the root password
+sudo file -s /dev/xvdf
 
 
+
+#Mounts external drive to /data folder
+sudo mkdir /data
+sudo mount /dev/xvdf /data
+
+
+#necessary to install things like dos2unix
 PATH=/sbin:$PATH
 export PATH
 
