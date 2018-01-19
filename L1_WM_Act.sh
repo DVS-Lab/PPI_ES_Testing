51070839b769758d3868f193f7e01a2932d32aac
Hyden Zhang <hyden.zhang@temple.edu>
2018-01-19 12:09:00 -0500

    changed exit file condition
    from a zstat image to a cope image since the working memory task template didn't generate post-stats output

--- L1_WM_Act.sh
+++ L1_WM_Act.sh
@@ -15,7 +15,7 @@ DATA=${MAINOUTPUTDIR}/${subj}/MNINonLinear/Results/tfMRI_${task}_${run}/smoothin
 NVOLUMES=`fslnvols ${DATA}`
 
 # checking L1 output
-if [ -e ${OUTPUT}.feat/cluster_mask_zstat1.nii.gz ]; then
+if [ -e ${OUTPUT}.feat/stats/cope2.nii.gz ]; then
   exit
 else
   rm -rf ${OUTPUT}.feat
